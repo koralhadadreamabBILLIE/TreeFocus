f8453a620d083440da31fa75462e6368ee8a1801
koralhamodgmail.com <koralhamoudd@gmail.com>
2025-11-04 11:39:44 +0200

    sdfsdf

--- app/src/main/java/nd/com/example/treefocus/MainActivity.java
+++ app/src/main/java/nd/com/example/treefocus/MainActivity.java
@@ -1,6 +1,11 @@
 package nd.com.example.treefocus;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView imv;
+    private TextView tvWelcome;
+    private TextView tvEmail;
+
+    private EditText etEmail;
+
+  private TextView tvPassword;
+  private EditText etPassword;
+  private Button btnSignin;
+
 
     /// ljhkjh;lijl;kjlkjlk
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imv = findViewById(R.id.imv);
+        tvWelcome = findViewById(R.id.tvWelcome);
+        tvEmail = findViewById(R.id.tvEmail);
+        etEmail = findViewById(R.id.etEmail);
+        tvPassword = findViewById(R.id.tvPassword);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignin = findViewById(R.id.btnSignin);
     }
 }
